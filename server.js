9031860122ed783f3cd43a6d87533bbbe7b4c4e4
SAMEERSMR <37589587+SAMEERSMR@users.noreply.github.com>
2020-10-27 00:24:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+let express = require('express');
+let app = express();
+const path = require('path');
+let nodemailer = require('nodemailer');
+const router = express.Router();
+
+
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+// step1
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+	port: 587,
+    secure: false,
+    auth: {
+        user: "sameermathan7@gmail.com",
+		pass: "9777620014a" 
+    }
+})
+
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+
+router.post('/access', (req, res, next) => {
+  var email = req.body.email
+  var message = req.body.message
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  var mail = {
+    from: name,
+    to: name,
+    message: subject,
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.info(`server has started on ${PORT}`))
